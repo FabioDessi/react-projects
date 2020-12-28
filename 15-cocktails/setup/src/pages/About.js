7f92840a615971284a1e6ebfb50eb4e147d90236
Fabio Dessi <fdessi@red.software.systems>
2020-12-28 03:18:43 +0100

    15-cocktails done

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi distinctio dolorum ea eaque eius, eos error eveniet, exercitationem illum ipsa libero nemo nesciunt nulla numquam porro, repellendus soluta ullam vitae.</p>
+    </section>
   )
 }
 
